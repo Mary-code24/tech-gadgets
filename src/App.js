1d91a441151554cfea6ff2bef76ca7ce3cc53e84
Mary-code24 <marinamar903@gmail.com>
2024-11-11 10:39:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [gifts, setGifts] = useState(data);
+  const [showText, setShowText] = useState(false);
+  
+  const removeGift = (id) => {
+    let newGift = gifts.filter(gifts => gifts.id !== id);
+
+    setGifts(newGift)
+  }
+
+  const setShowTextClick = (element) => {
+    element.showMore = !element.showMore;
+     setShowText(!showText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+      <h1>the {gifts.length}  best tech gifts</h1>
+      </div>
+
+{gifts.map((element => {
+  const{ id, gift, description, image,showMore } = element;
+
+  return ( <div key={id}>
+<div className='container'>
+<h2> {id} -  {gift} </h2>
+</div>
+
+<div className='container'>
+<img src= {image} width="300px" alt="gadgets"/>
+</div>
+
+<div className='container'>
+<p>{ showMore ? description : description.substring(0, 180) }
+<button onClick={() => setShowTextClick(element)}>{showMore ? "Show less" : "Show more"}</button>
+</p>
+</div>
+
+<div className='container'>
+<button className='btn' onClick={() => removeGift(id)}>remove</button>
+</div>
+
+  </div>
+
+  )
+}))}
+
+<div className='container'>
+<p><button onClick={() => setGifts([])}>Delete All</button>
+</p>
+</div>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    { 
+id: 1,
+gift: "Amazon Kindle",
+description: "Amazon's newest Kindle took our e-reader testing by storm. It's easy to use, with a crisp display and a solid price point. It boasts weeks of battery life and comes equipped with enough storage to allow you to store and read up to 16 GB of books",
+image: "https://m.media-amazon.com/images/I/61k4+E0x1YL._AC_SY450_.jpg",
+showMore: false
+    },
+
+    {
+    id: 2,
+    gift: "iRobot Roomba j9+: For the busy bee",
+    description: "If the person on your list often laments their lack of time for cleaning, the iRobot Roomba j9+ might make a great gift. The j9+ replaced the Roomba j7+ as our favorite robot vacuum thanks to its intuitive controls, stellar cleaning abilities, and top-notch navigation.",
+    image: "https://m.media-amazon.com/images/I/81HdMXcIYTL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    showMore: false
+    },
+
+    {
+        id: 3,
+        gift: "Petcube Cam: For the one who's crazy about their pets",
+        description: "This camera packs a lot of useful features into a device that squarely fits in the palm of your hand. It doesn't come with fancy extras like treat tossing or laser games like some competitors have. Still, it’s got everything you need to check in on your furry friend from anywhere, via your smartphone or tablet.",
+        image: "https://m.media-amazon.com/images/I/61EuxUPJi9L._AC_SY355_.jpg",
+        showMore: false
+    },
+
+    {
+    id: 4,
+    gift: "32ºN Adaptive Glasses: For the glasses wearer",
+    description: "Know someone who can't keep track of all their pairs of glasses? Treat them to the oh-so-splurgeworthy 32ºN adaptive focus glasses. With just the swipe of a finger on the lens, your gift recipient will be able to change the lens magnification and shift between normal glasses, reading glasses, and sunglasses, so their eyes stay ready for any kind of activity.",
+    image: "https://reviewed-com-res.cloudinary.com/image/fetch/s--u9GTUNUy--/b_white,c_fill_pad,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_220,q_auto,w_220/https://reviewed-production.s3.amazonaws.com/attachment/49ab5863d31e4ca8/32N.png",
+    showMore: false
+    },
+
+    {
+        id: 5,
+        gift: "Jall Sunrise Alarm Clock: For the one who needs a better alarm clock",
+        description: "When we tested the best sunrise alarm clocks, we were pleasantly surprised by the affordable Jall Sunrise Alarm Clock. It does an awesome job emulating sunrise and sunset, and works beautifully for the price.",
+        image: "https://reviewed-com-res.cloudinary.com/image/fetch/s--BuEhWtrm--/b_white,c_fill_pad,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_220,q_auto,w_220/https://reviewed-production.s3.amazonaws.com/1695319307156/Jall_alarm_clock.png",
+        showMore: false
+    },
+
+    {
+id: 6,
+gift: "Netvue Birdfy Feeder AI: For your favorite birdwatcher",
+description: "Your favorite songbird’s new favorite hangout comes equipped with a camera that takes gorgeous pictures and videos of the squirrels and birds that drop by for a snack. While the Netvue Birdfy Feeder AI doesn’t have the best AI identification system for the birds you’ll see, it’s still our favorite smart bird feeder because of its solar power and multiple mounting options.",
+image: "https://m.media-amazon.com/images/I/71s-TNSw+tL._AC_SY450_.jpg",
+showMore: false
+    }
+
+]
